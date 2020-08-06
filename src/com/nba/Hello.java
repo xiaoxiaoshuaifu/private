82a2e6ddf4b870f77a41f550f98c6f3a1085f0a2
zh_global <zh_git_global@qq.com>
2020-08-06 17:44:55 +0800

    测试私有仓库提交

---
+++ src/com/nba/Hello.java
@@ -0,0 +1,11 @@
+package com.nba;
+
+/**
+ * @auther xiaoxiaoshuaifu
+ * @date 2020/8/6 17:38
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello,world!");
+    }
+}
